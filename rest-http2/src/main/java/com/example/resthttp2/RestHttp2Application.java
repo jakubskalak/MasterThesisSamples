d02a5a1ccf10cffd6ea782a288c51e7f2a7ad414
jakubskalak <jakub.skalak@gmail.com>
2019-10-17 00:34:09 +0200

    first commit

---
+++ rest-http2/src/main/java/com/example/resthttp2/RestHttp2Application.java
@@ -0,0 +1,50 @@
+package com.example.resthttp2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.HandlerMapping;
+import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.server.WebSocketService;
+import org.springframework.web.reactive.socket.server.support.HandshakeWebSocketService;
+import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
+import org.springframework.web.reactive.socket.server.upgrade.ReactorNettyRequestUpgradeStrategy;
+import org.springframework.web.reactive.socket.server.upgrade.TomcatRequestUpgradeStrategy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+public class RestHttp2Application {
+
+	@Autowired
+	private WebSocketHandler webSocketHandler;
+
+	@Bean
+	public HandlerMapping webSocketHandlerMapping() {
+		Map<String, WebSocketHandler> map = new HashMap<>();
+		map.put("/event-emitter", webSocketHandler);
+
+		SimpleUrlHandlerMapping handlerMapping = new SimpleUrlHandlerMapping();
+		handlerMapping.setOrder(1);
+		handlerMapping.setUrlMap(map);
+		return handlerMapping;
+	}
+
+	@Bean
+	public WebSocketHandlerAdapter handlerAdapter() {
+		return new WebSocketHandlerAdapter(webSocketService());
+	}
+
+	@Bean
+	public WebSocketService webSocketService() {
+		return new HandshakeWebSocketService(new TomcatRequestUpgradeStrategy());
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestHttp2Application.class, args);
+	}
+
+}
